9cdca27f75b7532e7c0339c2e6c867dfa5245d5e
Benbob02 <91492582+Benbob02@users.noreply.github.com>
2021-10-25 15:36:10 -0600

    Snake done

--- 07-snake/Food.cs
+++ 07-snake/Food.cs
@@ -2,6 +2,24 @@ using System;
 
 namespace _07_snake
 {
-    // TODO: Define the Food class here.
+    public class Food : Actor
+    {
+        private int _food;
+        
+        public Food()
+        {
+            Random rnd = new Random();
+            _position = new Point(rnd.Next(0,600),rnd.Next(0,400));
+            _velocity = new Point(rnd.Next(0,600),rnd.Next(0,400));
+        }
+        public int GetPoints()
+        {
+            return 50;
+        }
+        public void Reset()
+        {
+            MoveNext();
+        }
+    }
  
 }
